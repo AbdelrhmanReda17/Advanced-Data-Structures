a65c3cb9c7260a60cd95a3499735fc9b7730fbd0
Abdelrhman R,Muhammed <11410120210225@stud.cu.edu.eg>
2023-12-07 16:14:15 +0200

    Upload the assignment

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,324 @@
+#include<bits/stdc++.h>
+#define OATHKEEPER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n"
+#define fr(x,n,o) for(ll i = x ; i < n ;i+=o)
+#define frj(x,n,o) for(ll j = x ; j < n ;j+=o)
+#define all(x) (x).begin(), (x).end()
+#define in(x,n) for(int i=0;i<(n);i++) cin>>(x)[i]
+#define out(x,n) for(int i=0;i<(n);i++) cout<<(x)[i]<<" \n"[i==(n)-1];
+using namespace std;
+template <typename T>
+class Node{
+    public:
+        Node* next;
+        T data;   
+    Node(T data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+template <typename T>
+class SingleLinkedList{
+    private:
+        Node<T>* head;
+        Node<T>* tail;
+        int len = 0;
+    public:
+        Node<T>* getHead(){
+            return head;
+        }
+        void setHead(Node<T>* head){
+            this->head = head;
+        }
+        SingleLinkedList() {
+            head = NULL;
+            tail = NULL;
+        }
+        void insertAtTail(T data)        // O(1)
+        {
+            Node<T>* Newitem = new Node<T>(data);
+            if(head == NULL){
+                tail = Newitem;
+                head = Newitem;
+                return;
+            }           
+            len +=1;
+            tail->next = Newitem;
+            tail = Newitem;
+        }
+        void insertAtHead(T data)         // O(1)
+        {
+            Node<T>* Newitem = new Node<T>(data);
+            if(head == NULL){
+                tail = Newitem;
+                head = Newitem;
+                return;
+            }
+            len +=1;
+            Newitem->next = head;
+            head = Newitem;
+        }
+        void insertAt(int index ,T data )         // O(n)
+        {
+            Node<T>* Newitem = new Node<T>(data);
+            if (index == 1){
+                insertAtHead(data);
+                return;
+            }else if(index-1 == len){
+                insertAtTail(data);
+                return;
+            }else if (index > len || index <= 0){
+                throw out_of_range("Index out of range");
+            }else if(len == 0)
+                throw invalid_argument("LinekedList Empty");
+            else{
+                Node<T>* prv = head;
+                int x =1;
+                len++;
+                while (x != index - 1 ) {
+                        prv = prv->next;
+                        x++;
+                }  
+                Newitem->next = prv->next;
+                prv->next = Newitem;
+            }
+        }
+        void print() // O(n)
+        {
+            Node<T>* temp = head;
+            if (head == NULL) {
+                cout << "List empty" << endl;
+                return;
+            }
+            while (temp != NULL) {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }cout << endl << "Size Of The linked List : " << len << endl ;
+        }
+        void removeAt(int index)  // O(n)
+        {
+            if(len == 0){
+                throw invalid_argument("LinekedList Empty");
+            }
+            if (index == 1){
+                removeAtHead();
+                return;
+            }else if(index-1 == len){
+                removeAtTail();
+                return;
+            }else if (index > len || index <= 0){
+                throw out_of_range("Index out of range");
+            }
+            else{
+                Node<T>* temp;
+                Node<T>* prv = head;
+                int x =1;
+                len--;
+                while (x != index - 1 ) {
+                        prv = prv->next;
+                        x++;
+                }  
+                temp = prv->next;
+                prv->next = temp->next;
+                delete[] temp;                
+            }
+        }
+        void removeAtTail(){  // O(n)
+            if(head == NULL)
+            {
+                cout << "List Empty" << endl;
+                return;
+            }
+            len--;
+            Node<T>* Newitem = head;
+            while (Newitem->next->next != NULL) {
+                Newitem = Newitem->next;
+            }            
+            delete tail;
+            Newitem->next = NULL;
+            tail = Newitem;
+        }
+        void removeAtHead(){  // O(1)
+            if(head == NULL)
+            {
+                cout << "List Empty " << endl;
+                return;
+            }
+            len--;
+            Node<T>* Newitem = head->next;
+            delete head;
+            head = Newitem;
+        }
+        bool isExist(T data) // O(n)
+        {
+            if(len == 0 )
+                throw invalid_argument("LinkedList Empty");
+            Node<T>* Newitem = head;
+            while(Newitem->data != data && Newitem->next != NULL)
+            {
+                Newitem = Newitem->next;
+            }
+            return (Newitem->data == data);
+        }
+        T retrieveAt(int index) // O(n)
+        {
+            if(index > len || index < 0 ){
+                throw out_of_range("Index out of range");
+            }
+            else if (len == 0)
+            {
+                throw invalid_argument("LinkedList Empty");
+            }
+            Node<T>* Newitem = head;
+            while(index != 0 && Newitem->next != NULL)
+            {
+                Newitem = Newitem->next;
+                index--;
+            }
+            return (Newitem->data);
+        }
+        int isItemAtEqual(T data , int index) // O(n)
+        {
+            if(index > len || index < 0 ){
+                throw out_of_range("Index out of range");
+            }
+            else if (len == 0)
+            {
+                throw invalid_argument("LinkedList Empty");
+            }
+            Node<T>* Newitem = head;
+            while(Newitem->next != NULL && index > 0)
+            {
+                Newitem = Newitem->next;
+                index--;
+            }
+            return (Newitem->data == data) ;
+        }
+        void replaceAt(T data , int index) // O(n)
+        {
+            Node<T>* Newitem = head;
+            while(Newitem->next != NULL && index != 0)
+            {
+                Newitem = Newitem->next;
+                index--;
+            }
+            Newitem->data = data ;
+        }
+        int isEmpty(){
+            return (linkedListSize() == 0);
+        }
+        int linkedListSize(){
+            return len - 1 == -1 ? 0 : len ;
+        }
+        void clear(){
+            if(len == 0 )
+                throw invalid_argument("LinkedList Empty");
+            len=0;
+            Node<T>* current = head;
+            while (current != nullptr) {
+                Node<T>* temp = current;
+                current = current->next;
+                delete temp;
+            }
+            head = nullptr;
+        }
+        void swap(int FirstItemindex, int Seconditemindex) {    //O(n)
+                if (FirstItemindex > len || FirstItemindex < 0) {
+                    throw out_of_range("Index out of range");
+                }
+                if (Seconditemindex > len || Seconditemindex < 0) {
+                    throw out_of_range("Index out of range");
+                }
+                if (len == 0) {
+                    throw invalid_argument("LinkedList Empty");
+                }
+                if (FirstItemindex > Seconditemindex) {
+                    std::swap(FirstItemindex, Seconditemindex);
+                }
+                if (FirstItemindex == Seconditemindex) {
+                    throw invalid_argument("Cannot Swap same index");
+                }
+                Node<T>* FirstNode = head;
+                Node<T>* SecondNode = head;
+                Node<T>* FirstNodePrev = NULL;
+                Node<T>* SecondNodePrev = NULL;
+                Node<T>* FirstNodeNext = NULL;
+                Node<T>* SecondNodeNext = NULL;
+                if (FirstItemindex == 0) {
+                    FirstNodePrev = NULL;
+                } else {
+                    while (FirstNode->next != NULL && FirstItemindex > 1) {
+                        FirstNode = FirstNode->next;
+                        FirstItemindex--;
+                    }
+                    FirstNodePrev = FirstNode;
+                    FirstNode = FirstNode->next;
+                }
+                if (Seconditemindex == 0) {
+                    SecondNodePrev = NULL;
+                } else {
+                    while (SecondNode->next != NULL && Seconditemindex > 1) {
+                        SecondNode = SecondNode->next;
+                        Seconditemindex--;
+                    }
+                    SecondNodePrev = SecondNode;
+                    SecondNode = SecondNode->next;
+                }
+                if (FirstNode == NULL || SecondNode == NULL) {
+                    return;
+                }
+                FirstNodeNext = FirstNode->next;
+                SecondNodeNext = SecondNode->next;
+                if (FirstNodeNext == SecondNode) { // Adjacent
+                    if (FirstNodePrev != NULL) {
+                        FirstNodePrev->next = SecondNode;
+                    } else {
+                        head = SecondNode;
+                    }
+                    SecondNode->next = FirstNode;
+                    FirstNode->next = SecondNodeNext;
+                    if (SecondNodeNext == NULL) {
+                        tail = FirstNode;
+                    }
+                    return;
+                } else if (SecondNodeNext == FirstNode) { // Adjacent
+                    if (SecondNodePrev != NULL) {
+                        SecondNodePrev->next = FirstNode;
+                    } else {
+                        head = FirstNode;
+                    }
+                    FirstNode->next = SecondNode;
+                    SecondNode->next = FirstNodeNext;
+                    if (FirstNodeNext == NULL) {
+                        tail = SecondNode;
+                    }
+                    return;
+                } else { // Not Adjacent
+                    if (FirstNodePrev != NULL) {
+                        FirstNodePrev->next = SecondNode;
+                    } else {
+                        head = SecondNode;
+                    }
+                    SecondNode->next = FirstNodeNext;
+                    if (SecondNodeNext == NULL) {
+                        tail = FirstNode;
+                    }
+                    if (SecondNodePrev != NULL) {
+                        SecondNodePrev->next = FirstNode;
+                    } else {
+                        head = FirstNode;
+                    }
+                    FirstNode->next = SecondNodeNext;
+                    if (FirstNodeNext == NULL) {
+                        tail = SecondNode;
+                    }
+                    return;
+                }
+            }
+    };
--- suffix_tree.cpp
+++ suffix_tree.cpp
@@ -1,34 +1,162 @@
+#include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-
+#include "SingleLinkedList.cpp"
 using namespace std;
 
-/**
- * Node Class
- * Represents a node in the suffix tree
-*/
-struct  Node {
-    
-    Node* children;  // Linked list of children nodes
-    int startEdge;   // Starting index of characters on the edge going to this node
-    int startSuffix; // Starting index of the suffix ending at this node (used for leaf nodes only)
+struct SuffixNode {
+    SingleLinkedList<SuffixNode*> children; // Linked list of children nodes
+    int startEdge;                          // Starting index of characters on the edge going to this node
+    int startSuffix;                        // Starting index of the suffix ending at this node (used for leaf nodes only)
 
-    /**
-     * Create a new Node object.
-     *
-     * @param startEdge the starting edge of the node
-     * @param startSuffix the starting suffix of the node
-     *
-     * @return a pointer to the newly created Node object
-     *
-     */
-    static Node* createNode(int startEdge = -1, int startSuffix = -1) {
-        Node* node = new Node; // Create a new Node object
-        node->children = nullptr; // Set the children pointer to nullptr
-        node->startEdge = startEdge; // Set the startEdge property of the node
-        node->startSuffix = startSuffix; // Set the startSuffix property of the node
-        return node; // Return a pointer to the newly created Node object
+    static SuffixNode* createNode(int startEdge, int startSuffix ) {
+        SuffixNode* node = new SuffixNode;
+        node->startEdge = startEdge;
+        node->startSuffix = startSuffix;
+        node->children = SingleLinkedList<SuffixNode*>();
+        node->children.insertAtTail(node);
+        return node;
+    }
+
+    static void addChild(SuffixNode* parent, SuffixNode* child) {
+        parent->children.insertAtTail(child);
     }
 };
+
+class SuffixTree {
+private:
+    SuffixNode* root;
+    char* str;
+public:
+    SuffixTree(const char* str) {
+        this->str = strdup(str);
+        this->root = this->buildSuffixTree(this->str);
+    }
+    void traverseSuffixes(SuffixNode* node) {
+        if (node->startSuffix != -1 && node->children.isEmpty()) {
+            cout << "Node: " << node->startEdge << " " << node->startSuffix << endl;
+            return;
+        }
+        Node<SuffixNode*>* current = node->children.getHead(); 
+        for(int i = 0; i < current->data->children.linkedListSize(); i++) {
+            SuffixNode* temp = current->data->children.retrieveAt(i+1);
+            if(temp == current->data && temp == nullptr){
+                continue;
+            }
+            traverseSuffixes(temp);
+        }
+        current = current->next;
+    }
+
+    SuffixNode* findSuffixNode(SuffixNode* node , int key) {
+        for(int i = 0; i < node->children.linkedListSize(); i++) {
+            SuffixNode* temp = node->children.retrieveAt(i+1);
+            if(temp->startEdge == key){
+                return temp;
+            }
+        }
+        return nullptr;
+    }
+    int getMinLength(SuffixNode* node) {
+        int min = INT16_MAX;
+        for(int i = 0; i < node->children.linkedListSize(); i++) {
+            SuffixNode* temp = node->children.retrieveAt(i+1);
+            if(temp->startEdge < min){
+                min = temp->startEdge;
+            }
+        }
+        return min;
+    }
+    // startSuffix -1
+    // banana$
+    // index 4 , i = 3
+    // i = 1
+    // index = 3
+    void walkDown(SuffixNode* node , int index , int length) {
+        if(node->startSuffix == -1){
+            int min = getMinLength(node);
+            bool found = true;
+            // BUG Here
+            for(int i = node->startEdge ; i <= min ; i++) {
+                if(str[i] == str[index]) {
+                    index++;
+                }else{
+                    found = false;
+                    SuffixNode* newNode1 = SuffixNode::createNode(i, length - 1 );
+                    SuffixNode::addChild(newNode1, node);
+                }
+            }
+            if(found){
+                int FirstOccurrence = getFirstOccurrence(node ,  index + 1 );
+                if (FirstOccurrence != -1) {
+                    for(int i = 0 ; i < node -> children.linkedListSize(); i++) {
+                        SuffixNode* walkDownNode = node->children.retrieveAt(i+1);
+                        if(str[walkDownNode->startEdge] == str[index]){
+                            walkDown(walkDownNode , index + 1 , length);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+        int i = node->startEdge;
+        while(i < strlen(str)) {
+            if(str[i] == str[index]) {
+                index++;
+                i++;
+                continue;
+            }else{
+                SuffixNode* newNode1 = SuffixNode::createNode(index, length - 1 );
+                SuffixNode* newNode2 = SuffixNode::createNode(i, node->startSuffix);
+                SuffixNode::addChild(node, newNode1);
+                SuffixNode::addChild(node, newNode2);
+                node->startSuffix = -1;
+                break;
+            }
+        }
+    }
+    // bana$
+    void insertIntoSuffix(SuffixNode* node, char* str , int index, int length ) {
+        int FirstOccurrence = getFirstOccurrence(node, index );
+        if (FirstOccurrence == -1) {
+            SuffixNode* newNode = SuffixNode::createNode(index, length - 1);
+            SuffixNode::addChild(node, newNode);
+            // 3nodes => leafs
+        }else{
+            for(int i = 0 ; i < node -> children.linkedListSize(); i++) {
+                SuffixNode* walkDownNode = node->children.retrieveAt(i+1);
+                if(str[walkDownNode->startEdge] == str[index]){
+                    walkDown(walkDownNode , index , length);
+                    // node a , 3 , 5
+                }
+            }
+        }
+        return;
+    }
+    SuffixNode* buildSuffixTree(char* str) {
+        SuffixNode* root = SuffixNode::createNode(-1, -1);
+        for (int i = 0; i < strlen(str); i++) {
+            insertIntoSuffix(root,str,i,strlen(str));
+        }
+        return root;
+    }
+    int getFirstOccurrence(SuffixNode* node , int index) {
+        for(int i = 0 ; i < node->children.linkedListSize(); i++) {
+            SuffixNode* temp = node->children.retrieveAt(i+1);
+            if(str[temp->startEdge] == str[index]) {
+                return temp->startEdge;
+            }
+        }
+        return -1;
+    }
+    SuffixNode* getRoot() {
+        return root;
+    }
+};
+
+int main() {
+    SuffixTree st("bananabana$");
+    st.traverseSuffixes(st.getRoot());
+    return 0;
+}
